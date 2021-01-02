30e7bbeeeecd22aaaa1101607b1dee454f63b985
emilieDelsol <emiliedelsol1@gmail.com>
2021-01-02 21:18:35 +0100

    [+] #2: add 16 route

--- APIMilhieCreation/Controllers/MilhieCreationController.cs
+++ APIMilhieCreation/Controllers/MilhieCreationController.cs
@@ -65,13 +65,75 @@ namespace APIMilhieCreation.Controllers
 			products = from a in _context.Products where a.UnderCategory.UnderCategoryId == 6 select a;
 			return products;
 		}
-
-		[HttpGet("[controller]/Jewerly")]
-		public IEnumerable<Product> GetJewerly()
+		[HttpGet("[controller]/Rings")]
+		public IEnumerable<Product> GetRings()
 		{
 			IEnumerable<Product> products = new List<Product>();
-			products = from a in _context.Products select a;
+			products = from a in _context.Products where a.UnderCategory.UnderCategoryId == 7 select a;
 			return products;
 		}
+		[HttpGet("[controller]/Earrings")]
+		public IEnumerable<Product> GetEarrings()
+		{
+			IEnumerable<Product> products = new List<Product>();
+			products = from a in _context.Products where a.UnderCategory.UnderCategoryId == 8 select a;
+			return products;
+		}
+
+		[HttpGet("[controller]/BraceletsFins")]
+		public IEnumerable<Product> GetBraceletsFins()
+		{
+			IEnumerable<Product> products = new List<Product>();
+			products = from a in _context.Products where a.UnderCategory.UnderCategoryId == 9 select a;
+			return products;
+		}
+		
+		[HttpGet("[controller]/Manchettes")]
+		public IEnumerable<Product> GetManchettes()
+		{
+			IEnumerable<Product> products = new List<Product>();
+			products = from a in _context.Products where a.UnderCategory.UnderCategoryId == 10 select a;
+			return products;
+		}
+		
+		[HttpGet("[controller]/BraceletLyto")]
+		public IEnumerable<Product> GetBraceletLyto()
+		{
+			IEnumerable<Product> products = new List<Product>();
+			products = from a in _context.Products where a.UnderCategory.UnderCategoryId == 11 select a;
+			return products;
+		}
+		
+		[HttpGet("[controller]/BraceletFin")]
+		public IEnumerable<Product> GetBraceletFin()
+		{
+			IEnumerable<Product> products = new List<Product>();
+			products = from a in _context.Products where a.UnderCategory.UnderCategoryId == 12 select a;
+			return products;
+		}
+		[HttpGet("[controller]/Necklace")]
+		public IEnumerable<Product> GetNecklace()
+		{
+			IEnumerable<Product> products = new List<Product>();
+			products = from a in _context.Products where a.UnderCategory.UnderCategoryId == 13 select a;
+			return products;
+		}
+		[HttpGet("[controller]/PendantChakras")]
+		public IEnumerable<Product> GetPendantChackras()
+		{
+			IEnumerable<Product> products = new List<Product>();
+			products = from a in _context.Products where a.UnderCategory.UnderCategoryId == 14 select a;
+			return products;
+		}
+		
+		[HttpGet("[controller]/PendantLyto")]
+		public IEnumerable<Product> GetPendantLyto()
+		{
+			IEnumerable<Product> products = new List<Product>();
+			products = from a in _context.Products where a.UnderCategory.UnderCategoryId == 15 select a;
+			return products;
+		}
+
+		
 	}
 }
